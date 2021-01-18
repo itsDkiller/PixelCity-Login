e5de3896f2b1dc9ee95c11b62caf3fb4cd3bc75f
itsDkiller <40144185+itsDkiller@users.noreply.github.com>
2021-01-18 12:35:48 +0100

    Added logger class

---
+++ logger/logger.js
@@ -0,0 +1,50 @@
+const colors     = require('colors');
+const dateformat = require('dateformat');
+
+class Logger {
+
+    /**
+     * @param {string} prefix Prefix to display after the timestamp
+     * @param {Date} date Node.js date instance
+     */
+    constructor(prefix, date) {
+        this.prefix = prefix;
+        this.date   = date;
+    }
+
+    /**
+     * @param {string} info Info message to display
+     * @param {string} location Location of the information message
+     */
+    displayInfo(location, info) {
+        return console.log(`[`.grey + `${this.parseDateToString()}`.white + `]`.grey + `[`.grey + `${this.prefix} `.green + `- `.grey + `INFO`.cyan + `] `.grey + `${location}: ${info}`.white);
+    }
+
+    /**
+     * 
+     * @param {string} warning Warning message to display
+     * @param {string} location Location of the warning message
+     */
+    displayWarning(location, warning) {
+        return console.log(`[`.grey + `${this.parseDateToString()}`.white + `]`.grey + `[`.grey + `${this.prefix} `.green + `- `.grey + `WARNING`.yellow + `] `.grey + `${location}: ${warning}`.white);
+    }
+
+    /**
+     * @param {string} error Error message to display
+     * @param {string} location Location of the error message
+     */
+    displayError(location, error) {
+        return console.log(`[`.grey + `${this.parseDateToString()}`.white + `]`.grey + `[`.grey + `${this.prefix} `.green + `- `.grey + `ERROR`.red + `] `.grey + `${location}: ${error}`.white);
+    }
+
+    /**
+     * @returns {string} Parsed date format
+     */
+    parseDateToString() {
+        return dateformat(this.date, "yyyy-mm-dd h:MM:ss");
+    }
+}
+
+module.exports = {
+    Logger
+}
